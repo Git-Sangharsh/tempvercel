9dffb277b90a3e54b76f2e7ccfa270d4d50c1bea
sangharsh <sangharshdev69@gmail.com>
2024-11-26 16:59:10 +0530

    added prod url

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@
 const nextConfig = {
   env: {
     // Change below URL with your current domain
-    API_PROD_URL: "http://localhost:3000/api",
-    storageURL: "http://localhost:3000",
+    API_PROD_URL: "tempvercel-delta.vercel.app/api",
+    storageURL: "tempvercel-delta.vercel.app",
   },
 
   images: {
