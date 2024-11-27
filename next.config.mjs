c6370014c049978771fa645243c95b7422a27a61
sangharsh <sangharshdev69@gmail.com>
2024-11-27 15:22:26 +0530

    resolving the issue

--- next.config.mjs
+++ next.config.mjs
@@ -8,15 +8,12 @@ const nextConfig = {
   },
 
   images: {
-    // Adding domains for external images
-    domains: ["tempvercel-delta.vercel.app"],
-
-    // Adding remote patterns for image optimization
     remotePatterns: [
       {
         protocol: "https",
         hostname: "tempvercel-delta.vercel.app",
       },
+      ,
       {
         protocol: "http",
         hostname: "127.0.0.1",
