9fd3b398f8963247fed6b21c812c3bba676b1c4c
sangharsh <sangharshdev69@gmail.com>
2024-11-27 13:58:29 +0530

    api commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     // Change below URL with your current domain
     API_PROD_URL: "https://tempvercel-delta.vercel.app/api",
-    storageURL: "https://tempvercel-delta.vercel.app/",
+    storageURL: "https://tempvercel-delta.vercel.app/api",
   },
 
   images: {
